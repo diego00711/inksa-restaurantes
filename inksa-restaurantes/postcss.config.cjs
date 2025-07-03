877fb4cb9e0f68db5a6cebbb735c49f89660d035
Diego <ddiegowweber@gmail.com.br>
2025-07-03 20:39:33 -0300

    feat: Adiciona .gitignore e corrige dependencias

--- inksa-restaurantes/postcss.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {}, // Para Tailwind CSS v3.x, usa tailwindcss: {} diretamente
-    autoprefixer: {},
-    cssnano: {} // Opcional: para minificação, se não estiver usando cssMinify no Vite
-  },
-};
