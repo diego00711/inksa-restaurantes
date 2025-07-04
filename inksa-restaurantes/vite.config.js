f791d9b2d8420830cb21b9dff48c55f109e0da05
diego00711 <ddiegowweber@gmail.com>
2025-07-04 14:57:27 -0300

    Update vite.config.js

--- inksa-restaurantes/vite.config.js
+++ inksa-restaurantes/vite.config.js
@@ -1,9 +1,13 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// A configuração do PostCSS foi movida para um arquivo separado (postcss.config.js)
-// para garantir a máxima compatibilidade.
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
