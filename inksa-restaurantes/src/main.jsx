bbbd8b415ba33cfeebeb57f594ad06eaf708b870
diego00711 <ddiegowweber@gmail.com>
2025-07-04 15:29:19 -0300

    Update main.jsx

--- inksa-restaurantes/src/main.jsx
+++ inksa-restaurantes/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // Importa os estilos do Tailwind para toda a aplicação
+import './index.css' // Esta linha é crucial para importar os estilos do Tailwind.
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
