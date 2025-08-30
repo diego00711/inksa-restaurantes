d45d7a9cf30a26d3bd1919be82b436d8ec1da7c9
diego00711 <ddiegowweber@gmail.com>
2025-08-30 09:42:53 -0300

    Add restaurant review service functions

---
+++ src/services/restaurantReviewsService.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:5000/api/review";
+
+export async function postRestaurantReview({
+  restaurantId,
+  orderId,
+  rating,
+  comment,
+  token,
+}: {
+  restaurantId: string;
+  orderId: string;
+  rating: number;
+  comment?: string;
+  token: string;
+}) {
+  const res = await axios.post(
+    `${API_BASE}/restaurants/${restaurantId}/reviews`,
+    { order_id: orderId, rating, comment },
+    { headers: { Authorization: `Bearer ${token}` } }
+  );
+  return res.data;
+}
+
+export async function getRestaurantReviews(restaurantId: string) {
+  const res = await axios.get(`${API_BASE}/restaurants/${restaurantId}/reviews`);
+  return res.data; // {reviews, average_rating, total_reviews}
+}
